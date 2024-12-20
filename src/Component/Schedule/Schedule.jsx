efa286d482c25ef1849cf7a1a5f65af475e09a30
Harshitver0511 <154688128+Harshitver0511@users.noreply.github.com>
2024-12-20 18:52:53 +0530

    final schedule updated

--- src/Component/Schedule/Schedule.jsx
+++ src/Component/Schedule/Schedule.jsx
@@ -2,27 +2,22 @@ import React from 'react'
 import './Accepted.css'
 import img1 from "./1.png"
 import img2 from "./2.png"
+import fins from "./fins.pdf"
 
 
 function Schedule() {
   return (
     <>
-  
-    {/* <div className="accepted grid grid-cols-1 md:grid-cols-2 gap-5 mt-5 mx-3 md:mx-24">
-        
-          <Sidebar />
-     
-            <div className="nitp-notice">
-              <Notice />
-            </div>
-          
-        </div> */}
     
         <div className="mx-5 md:mx-20">
-        <div>
-          <img src={img1} />
-          <img src={img2} />
-        </div>
+         <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        src={fins}
+        width="100%"
+        height="100%"
+        title="PDF Viewer"
+      />
+    </div>
       </div>
    
     </>
